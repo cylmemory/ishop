151392a6ef67981463fe73a2175457ca57c12baa
cylmemory <524545756@qq.com>
2019-01-14 22:32:50 +0800

    models

---
+++ apps/users/models.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    name = models.CharField(max_length=30, null=True, blank=True, verbose_name="姓名")
+    gender = models.CharField(max_length=6, choices=(("male", u"男"), ("female", u"女")), default="male", verbose_name="性别")
+    mobile = models.CharField(max_length=11, verbose_name="联系方式")
+    birth = models.DateField(null=True, blank=True, verbose_name="生日")
+    email = models.CharField(max_length=100, null=True, blank=True, verbose_name="邮箱")
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self):
+        return self.name
+
+
+class VerifyMessage(models.Model):
+    """
+    用户验证码
+    """
+    code = models.CharField(max_length=10, verbose_name="验证码")
+    mobile = models.CharField(max_length=11, verbose_name="手机号码")
+    add_time = models.DateField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "手机验证码"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.code
+
